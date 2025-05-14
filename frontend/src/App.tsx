e397dab8074caae7bfcde8b2f03e40ef2422003a
FESTGROUP-extranet <i@festgroup.in>
2025-05-14 10:10:51 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
 // by GAETAN
+
+// i am deepak|
+
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
